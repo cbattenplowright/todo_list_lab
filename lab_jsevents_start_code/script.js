36cafe22b798ba8a8876f7dc450792e2a9e2f88d
cbattenplowright <callumbp15@gmail.com>
2023-08-29 22:35:41 +0100

    added complete task functionality

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -2,15 +2,26 @@
 
 const input = document.querySelector("#new-todo");
 const button = document.querySelector("#enter");
-const toDoList = document.querySelector("#list");
+const toDoList = document.querySelector("#to-do-list");
+const completedList = document.querySelector("#completed-list");
 const dateButton = document.querySelector("#date");
 const dateDisplay = document.querySelector("#date-display");
 
 // deletes parent node element
 const deleteParentNode = (element) => {
     console.log("parent node deleted");
-    toDoList.removeChild(element.parentNode);
+    // removes from list depending on which list it is in
+    switch (element.parentNode.parentNode) {
+        case toDoList:
+            toDoList.removeChild(element.parentNode);
+            break;
+        case completedList:
+            completedList.removeChild(element.parentNode);
+        default:
+            break;
+    }
 };
+
 
 // creates delete button and adds event listener
 const createDeleteButton = () => {
@@ -26,7 +37,23 @@ const createDeleteButton = () => {
     });
 
     return deleteButton;  
-}
+};
+
+// creates a complete checkbox and adds event listener
+const createCompleteButton = () => {
+    const completeButton = document.createElement("input");
+    completeButton.type = "checkbox";
+    completeButton.setAttribute("style", "margin-left: 0.5em");
+
+    completeButton.addEventListener("click", (event) => {
+        // moves the list item to completed list
+        console.log("complete button pressed");
+        completedList.appendChild(completeButton.parentNode);
+        completeButton.parentNode.setAttribute("style", "color: grey");
+    });
+
+    return completeButton;
+};
 
 // creates new list item when enter button is clicked
 button.addEventListener("click", (event) => {
@@ -34,6 +61,7 @@ button.addEventListener("click", (event) => {
     const newToDo = document.createElement("li");
     newToDo.innerText = input.value;
     newToDo.appendChild(createDeleteButton());
+    newToDo.appendChild(createCompleteButton());
     
     toDoList.appendChild(newToDo);
 });
